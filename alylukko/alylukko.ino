c2f663ddfa434aae7f71a0848031aef75aef0729
Henri Väisänen <henri.vaisane@gmail.com>
2019-09-26 13:43:58 +0300

    alylukko_with_broken_supermario_sound.ino
    Added a supermario theme that doesn't actually work (yet) for funzies :)

--- alylukko/alylukko.ino
+++ alylukko/alylukko.ino
@@ -1,8 +1,111 @@
 #include <Key.h>
 #include <Keypad.h>
 
+
+// TONES  ==========================================
+// Start by defining the relationship between
+//       note, period, &  frequency.
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+// Define a special note, 'R', to represent a rest
+#define  R     0
+
+
 const byte numRows = 4;
 const byte numCols = 4;
+
+int servoPin = 10;
+
+int ledPin = 13;
+
+int piezoPin = 11;
 
 char keymap[numRows][numCols] =
 {
@@ -27,41 +130,163 @@ void setup()
 {
   // put your setup code here, to run once:
   //KOODI JOLLA ALUSTETAAN ARDUINO (KOMPONENTIT YMS)
-  Serial.begin(9600);
+    pinMode(servoPin, OUTPUT);
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
+  	pinMode(piezoPin, OUTPUT);
 }
 
-void correctCode() 
+
+
+//int melody[] = {  C,  b,  g,  C,  b,   e,  R,  C,  c,  g, a, C };
+int melody[] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0
+};
+//int beats[]  = { 16, 16, 16,  8,  8,  16, 32, 16, 16, 16, 8, 8 };
+int beats[] = {
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
+int MAX_COUNT = sizeof(melody) / 2; // Melody length, for looping.
+
+// Set overall tempo
+long tempo = 10000;
+// Set length of pause between notes
+int pause = 1000;
+// Loop variable to increase Rest length
+int rest_count = 100; //<-BLETCHEROUS HACK; See NOTES
+
+// Initialize core variables
+int tone_ = 0;
+int beat = 0;
+long duration  = 0;
+
+void playTone() {
+  long elapsed_time = 0;
+  if (tone_ > 0) { // if this isn't a Rest beat, while the tone has
+    //  played less long than 'duration', pulse speaker HIGH and LOW
+    while (elapsed_time < duration) {
+
+      digitalWrite(piezoPin,HIGH);
+      delayMicroseconds(tone_ / 2);
+
+      // DOWN
+      digitalWrite(piezoPin, LOW);
+      delayMicroseconds(tone_ / 2);
+
+      // Keep track of how long we pulsed
+      elapsed_time += (tone_);
+    }
+  }
+  else { // Rest beat; loop times delay
+    for (int j = 0; j < rest_count; j++) { // See NOTE on rest_count
+      delayMicroseconds(duration);  
+    }                                
+  }                                
+}
+
+
+void correctCode()
 {
-    //JOS KOODI OIKEIN; SUORITETAAN TÄMÄ
-    //PITÄÄ LEDIN PÄÄLLÄ 5 SEKUNTIA
-    digitalWrite(13, HIGH);
+  	playTone(); // Send 1KHz sound signal...
+  	digitalWrite(ledPin, HIGH);
     delay(5000);
-    digitalWrite(13, LOW);
+    digitalWrite(ledPin, LOW);
+  
+  
+  	for (int i = 0; i<30; i++)
+    {
+    
+    digitalWrite(servoPin, HIGH);
+    delay(500);
+    digitalWrite(servoPin, LOW);
+    delay(20);
+    }
 }
 
 void incorrectCode()
 {
   //JOS KOODI ON VÄÄRÄ, SUORITETAAN TÄMÄ KOODI
-  //VILKUTTAA LEDIÄ 5 KERTAA PUOLEN SEKUNNIN AJAN PUOLEN SEKUNNIN VÄLEIN
-    for (int e = 0; e<5; e++)
+  	
+    for (int i = 0; i<5; i++)
     {
     
-	digitalWrite(13, HIGH);
+	digitalWrite(ledPin, HIGH);
 	delay(500);
-  	digitalWrite(13, LOW);
+  	digitalWrite(ledPin, LOW);
   	delay(500);
     }
 }
 
 void loop() //PÄÄKOODI
 {
+  
+  
+    for (int i=0; i<MAX_COUNT; i++) {
+    tone_ = melody[i];
+    beat = beats[i];
+
+    duration = beat * tempo; // Set up timing
+
+    playTone();
+    // A pause between notes...
+    delayMicroseconds(pause);
+    }
+  
+  
   //jos lukko on auki, odota sen sulkemista ennen jatkamista...
 
   for (int i = 0; i < 4; i++)
   {
     
     char key = myKeypad.waitForKey(); //SYÖTÄ...
-    Serial.println(key);
+    Serial.println(key);//testausta
+    correctCode(); //testausta
     int keyInt = key - '0';       //MUUTETAAN NUMEROKSI...
     if (pincode[i] == keyInt)     //JOS SYÖTETTY YKSITTÄINEN KOODINUMERO ON OIKEIN...
     {
@@ -71,9 +296,9 @@ void loop() //PÄÄKOODI
 
   if (correct == 4)
   { //JOS KOODI ON OIKEIN...
-    //TÄHÄN KOODI JOKA SUORITETAAN, KUN KOODI ON OIKEA...
       correctCode();
   } else {
+    //JOS KOODI ON VÄÄRÄ...
       incorrectCode();
   }
 }
